29dba3d5a6ff3d697846e042d0dd055f5e477297
Lorman Lau <lormanlau@gmail.com>
2017-07-03 20:43:01 -0700

    got video search working

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 $("#search_button").click(function(){
 	var search_words = $("#search_bar").val();
 	$.getJSON( "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + search_words + "&key=AIzaSyATUPsfY3MunXxd2f8nk2x0hFiJuq_bUKg" , function(obj){
-        console.log(obj);
+        var base = $("iframe").attr("src");
+        var video = obj.items[0].id.videoId;
+        console.log(video);
+        $("iframe").attr("src", base + video);
     });
+    return false;
 });
